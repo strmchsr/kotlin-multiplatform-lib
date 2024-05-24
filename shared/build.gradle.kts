a5dcc75f968477b44dbeca60350124d9bdb723ea
Shashank Srivastava <shashank.srivastava@agbeindia.com>
2024-05-24 17:50:18 +0530

    kmp poc

---
+++ shared/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinCocoapods)
+    alias(libs.plugins.androidLibrary)
+    id("maven-publish")
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+        publishLibraryVariants("release", "debug")
+    }
+    android {
+        publishLibraryVariants("release", "debug")
+    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    cocoapods {
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        version = "1.0.2"
+        ios.deploymentTarget = "16.0"
+        framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            //put your multiplatform dependencies here
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+android {
+    namespace = "com.strmchsr.fileuploaderlib"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+publishing {
+    publications {
+        create<MavenPublication>("greeting") {
+            from(components["kotlin"])
+            groupId = "com.strmchsr"
+            artifactId = "fileuploaderlib"
+            version = "1.0.2"
+        }
+    }
+    repositories {
+        maven {
+            url = uri("$buildDir/repo")
+        }
+    }
+}
